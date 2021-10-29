dfaeefa0c6b6e66f827fde0a8debeac4eb29b62d
Gleb S. Brykin <GlebSBrykin@yandex.ru>
2021-10-29 18:37:58 +0300

    Add files via upload

--- Implementation/src/Program.cs
+++ Implementation/src/Program.cs
@@ -8,9 +8,15 @@ using System.Windows.Forms;
 namespace ColorfulSoft.DeOldify
 {
 
+    /// <summary>
+    /// Main class.
+    /// </summary>
     public static class Program
     {
 
+        /// <summary>
+        /// Entry point.
+        /// </summary>
         [STAThread]
         public static void Main()
         {
